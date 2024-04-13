8838de40e1b51337d7196613210bd046f334858a
uniowlik <777ruspacan777@gmail.com>
2024-04-13 17:53:08 +0300

    feat: swipe emitter

--- Assets/_Dev/UniOwl/Scripts/AttackSwipeView.cs
+++ Assets/_Dev/UniOwl/Scripts/AttackSwipeView.cs
@@ -4,22 +4,24 @@ namespace Game.Character
 {
     public class AttackSwipeView : MonoBehaviour
     {
-        [SerializeField] private ParticleSystem _ps;
-
-        private void Start()
-        {
-            Emit(Vector3.one, 0f);
-            Emit(-Vector3.one, 180f);
-        }
+        [SerializeField] private ParticleSystem _psSwipe;
+        [SerializeField] private ParticleSystem _psImpact;
 
         public void Emit(Vector3 position, float rotationY)
         {
-            var emitParams = new ParticleSystem.EmitParams()
+            var emitParamsSwipe = new ParticleSystem.EmitParams()
             {
                 position = position,
                 rotation3D = new Vector3(0f, rotationY, 0f),
             };
-            _ps.Emit(emitParams, 1);
+            _psSwipe.Emit(emitParamsSwipe, 1);
+
+            var shape = _psImpact.shape;
+            shape.position = position;
+            shape.rotation = new Vector3(rotationY, 90f + Mathf.Lerp(-30f, 30f, rotationY / 180f), 0f);
+            
+            var emitParamsImpact = new ParticleSystem.EmitParams();
+            _psImpact.Emit(emitParamsImpact, 30);
         }
     }
 
