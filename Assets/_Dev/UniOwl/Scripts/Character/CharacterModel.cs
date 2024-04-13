0a8845582ef9db8b7be3eec9ed54b4d76daf7b11
uniowlik <777ruspacan777@gmail.com>
2024-04-13 16:30:29 +0300

    feat: basic character

---
+++ Assets/_Dev/UniOwl/Scripts/Character/CharacterModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Character
+{
+    public class CharacterModel
+    {
+        public float CurrentHealth { get; private set; }
+        public float MaxHealth { get; }
+        public float AttackDamage { get; }
+        public float AttackSpeed { get; }
+        public float WalkSpeed { get; }
+
+        public event Action<CharacterModel> OnHealthZero; 
+        
+        public void DealDamage(float damage)
+        {
+            CurrentHealth = Mathf.Clamp(CurrentHealth - damage, 0f, MaxHealth);
+            
+            if (CurrentHealth <= 0f)
+                OnHealthZero?.Invoke(this);
+        }
+    }
+}
---
+++ Assets/_Dev/UniOwl/Scripts/Character/CharacterStateMachine.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Game.Character
+{
+    public class CharacterStateMachine
+    {
+        private ICharacterState _currentState;
+
+        public ICharacterState CurrentState => _currentState;
+
+        private CharacterFacade _characterFacade;
+        public CharacterFacade CharacterFacade => _characterFacade;
+
+        public CharacterStateMachine(CharacterFacade facade)
+        {
+            _characterFacade = facade;
+        }
+        
+        public void SetState(ICharacterState state)
+        {
+            _currentState?.Exit();
+            _currentState = state;
+            _currentState?.Enter(this);
+        }
+    }
+}
