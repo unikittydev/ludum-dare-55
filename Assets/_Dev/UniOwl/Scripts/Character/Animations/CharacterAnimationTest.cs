1418af0c29e0d543f912e87356331c64761491d8
uniowlik <777ruspacan777@gmail.com>
2024-04-13 19:41:07 +0300

    feat: footsteps and anim events

--- Assets/_Dev/UniOwl/Scripts/Character/Animations/CharacterAnimationTest.cs
+++ Assets/_Dev/UniOwl/Scripts/Character/Animations/CharacterAnimationTest.cs
@@ -9,7 +9,12 @@ namespace Game.Character
         private static readonly int DEATH = Animator.StringToHash("DEATH");
 
         [SerializeField] private Animator _animator;
+        [SerializeField] private CharacterParticlesView characterParticlesView;
 
+        [SerializeField] private Transform _attackPivot;
+        [SerializeField] private Transform _impactPivot;
+        [SerializeField] private Transform _footstepPivot;
+        
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Q))
@@ -18,6 +23,21 @@ namespace Game.Character
                 _animator.SetTrigger(DAMAGE);
             if (Input.GetKeyDown(KeyCode.Space))
                 _animator.SetTrigger(DEATH);
+        }
+
+        public void AttackEvent(int value)
+        {
+            characterParticlesView.EmitSwipe(_attackPivot.position, transform.eulerAngles.y);
+        }
+        
+        public void DamageEvent(int value)
+        {
+            characterParticlesView.EmitImpact(_impactPivot.position, 180f + transform.eulerAngles.y);
+        }
+        
+        public void FootstepsEvent(float offsetX)
+        {
+            characterParticlesView.EmitFootsteps(_footstepPivot.position + new Vector3(offsetX, 0f, 0f), 180f + transform.eulerAngles.y);
         }
     }
 }
