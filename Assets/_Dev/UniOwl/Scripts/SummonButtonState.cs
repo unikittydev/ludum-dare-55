30d4955cc250286439b132dd609511df0aac1447
uniowlik <777ruspacan777@gmail.com>
2024-04-14 13:31:48 +0300

    feat: summon button

---
+++ Assets/_Dev/UniOwl/Scripts/SummonButtonState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Magic
+{
+    [Serializable]
+    public class SummonButtonState
+    {
+        public Color _outerTint = Color.white;
+        public Color _innerTint = Color.white;
+        public Color _handTint = Color.white;
+        
+        public float _outerScale = 3.5f;
+        public float _innerScale = 2.5f;
+        public float _handScale = 1f;
+    }
+}
